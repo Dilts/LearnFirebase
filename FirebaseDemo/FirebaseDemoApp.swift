9be17a6c7eb8f3187fdd296baef60e6b2ffda78c
Brian Dilts <brianedilts@gmail.com>
2021-04-19 10:33:09 -0600

    Initial Commit

---
+++ FirebaseDemo/FirebaseDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  FirebaseDemoApp.swift
+//  FirebaseDemo
+//
+//  Created by Brian Dilts on 4/19/21.
+//
+
+import SwiftUI
+
+@main
+struct FirebaseDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
